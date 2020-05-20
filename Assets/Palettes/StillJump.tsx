7f93b74f37684f122f11213f50956e27da774b7b
schmoecci <46814569+schmoecci@users.noreply.github.com>
2020-05-20 20:05:02 +0200

    Level_Layer_Test
    Background added
    Collision added
    Groundlayer added
    Player added
    Hitbox added

---
+++ Assets/Palettes/StillJump.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="StillJump" tilewidth="16" tileheight="16" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="gras_dirt_16x16.png"/>
+ </tile>
+</tileset>
