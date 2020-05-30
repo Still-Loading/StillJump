8d593d2c3099cb522beb9311a40dcf82e88590a0
Bolsei <t.rudersdorf@gmx.de>
2020-05-30 10:29:34 +0200

    Leben implementiert
    Lebensanzeige eingebaut.
    Ins Wasser Fallen zieht ein Leben ab.
    Nach vier Versuchen, kommt die GameOver Anzeige.
    Nachdem Tod spawnt man am Start oder Checkpoint

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    private LevelManager levelManager;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            levelManager.RespawnPlayer();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -11,8 +12,35 @@ public class LevelManager : MonoBehaviour
     //Spieler GameObject
     public GameObject player;
 
+    public Image noLiveImage;
+    public Image noLiveImage1;
+    public Image noLiveImage2;
+
+    public Text death;
+
+    public int live;
+
     public void RespawnPlayer()
     {
+        //Leben abziehen
+        live--;
+        switch(live)
+        {
+            case 2:
+                noLiveImage2.enabled = true;
+                break;
+            case 1:
+                noLiveImage1.enabled = true;
+                break;
+            case 0:
+                noLiveImage.enabled = true;
+                break;
+            default: death.enabled = true;
+                break;
+        }
+        
+
+        //Player an den Checkpoint setzen
         player.transform.position = currentCheckpoint.transform.position;
     }
 }
